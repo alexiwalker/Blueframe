f5920567680f683140c43a356a99a0d690ea4b5c
Alex Walker <alexander.i.walker@outlook.com>
2019-09-06 15:40:03 +1000

    updating the templater - support for recursively adding replacers inside replacers, and still updating them

--- lib/template/template.dart
+++ lib/template/template.dart
@@ -3,7 +3,6 @@ import 'package:blueframe/blueframe.dart';
 
 class Template {
 
-
   Template({this.replacers, this.template, basePath}) {
     if(basePath != "")
       this.basePath+="$basePath\\";
@@ -21,17 +20,19 @@ class Template {
   }
 
   Map<String, String> replacers;
+
   String template;
   String _templateContents;
   String basePath = "";
   String errString = "";
+
   List<String> includedFiles = [];
 
 
   final RegExp includePattern = RegExp(r'({{@include\s\"[A-Za-z0-9.\\\/]+\"}})', caseSensitive: true);
   final RegExp includeOncePattern = RegExp(r'({{@includeonce\s\"[A-Za-z0-9.\\\/]+\"}})', caseSensitive: true);
-  final RegExp replacersPattern = RegExp(r'(\[\[[A-Za-z0-9]+]\])');
-  final RegExp filenamesPattern = RegExp(r'(\"[A-Za-z0-9.\\\/]+\")');
+  final RegExp replacersPattern = RegExp(r'(\[\[[A-Za-z0-9]+]\])', caseSensitive: true);
+  final RegExp filenamesPattern = RegExp(r'(\"[A-Za-z0-9.\\\/]+\")', caseSensitive: true);
 
   void setReplacers(Map<String, String> replacers) {
     this.replacers = replacers;
@@ -54,6 +55,10 @@ class Template {
     }
   }
 
+  ///
+  /// Replacers are denoted by [[]] in the .dtp file
+  /// they are casesensitive
+  ///
   void runReplacers() {
     for (var pair in replacers.entries) {
       final String key = pair.key;
@@ -64,12 +69,43 @@ class Template {
     }
   }
 
+  ///
+  /// Attempts to replace all replacerfields so long as they exist
+  /// this may be desired where the replcers list values itself contains more replacers, leaving [[field]] after the first pass
+  ///
+  void forceReplacers() {
+    while (hasReplacers()){
+      var start = _templateContents;
+      for (var pair in replacers.entries) {
+        final String key = pair.key;
+        final String content = pair.value;
+        print(content);
+        final String formatter = "(\\[\\[$key\\]\\])";
+        _templateContents = _templateContents.replaceAllMapped(RegExp(formatter, caseSensitive: false), (Match m) => content);
+      }
 
+      //if a pass results in no change to the document, break the loop
+      if(_templateContents==start)
+        break;
+    }
+
+  }
+
+
+  ///
+  /// currently directives only includes... includes
+  /// it will contain more complicated functions/tags in the future
+  ///
+  /// Directives are denoted by {{}} in the .dtp file
+  ///
   void runDirectives() {
     runIncludes();
     runIncludeOnce();
   }
 
+  ///
+  /// Format: {{@include "filename.js"}}
+  ///
   void runIncludes(){
     while (hasIncludes()) {
       _templateContents = _templateContents.replaceAllMapped(includePattern, (Match m) {
@@ -91,6 +127,14 @@ class Template {
     }
   }
 
+  ///
+  /// any includeonce files are only included once per Template object, based on a List<String> of included filenames
+  ///
+  /// this may be bypassed by creating a second template object, rendering it to get contents and then appending those contents to another
+  /// ...although if you need to do that, maybe just use @include instead of @includeonce
+  ///
+  /// Format: {{@includeonce "filename.js"}}
+  ///
   void runIncludeOnce(){
     while (hasIncludeOnce()) {
       _templateContents = _templateContents.replaceAllMapped(includeOncePattern, (Match m) {
@@ -120,8 +164,8 @@ class Template {
 
   void appendFile(String filename) {
     try {
-      final File f = File( "$basePath/$filename");
-      final String content = f.readAsStringSync();
+      final File file = File( "$basePath/$filename");
+      final String content = file.readAsStringSync();
       _templateContents += "\n$content";
     } catch (e) {
       print(e.toString());
@@ -137,5 +181,5 @@ class Template {
     runDirectives();
     runReplacers();
   }
-  
+
 }
