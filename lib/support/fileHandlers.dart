a08887b4b0da8e257ea8ba274a05620470c6689c
alexander walker <alexander.i.walker@outlook.com>
2019-09-09 10:39:08 +1000

    Css files don't like being read as bytes like the other types in filehandlers, so gave it a read as string to use instead.
    made some changes to home.dhml and components.dhml to include and test a stylesheet

--- lib/support/fileHandlers.dart
+++ lib/support/fileHandlers.dart
@@ -15,7 +15,7 @@ class FileHandlers {
 
 // ignore: non_constant_identifier_names
 	static Future<Response> Css(Request request) async =>
-		await getFileResponse("assets/css/${request.path.segments.join("/")}");
+		await getStringResponse("assets/css/${request.path.segments.join("/")}");
 
 // ignore: non_constant_identifier_names
 	static Future<Response> Ico(Request request) async =>
@@ -25,6 +25,21 @@ class FileHandlers {
 		try {
 			final file = File(path);
 			final fileContents = await file.readAsBytes();
+			final contentType = mime.lookupMimeType(path);
+			return Response.ok(fileContents)
+				..contentType = io.ContentType.parse(contentType);
+		} catch (e) {
+			print(e);
+			return Response.notFound();
+		}
+	}
+
+
+	//.css files error when read as bytes...so I guess we read as string.
+	static Future<Response> getStringResponse(String path) async {
+		try {
+			final file = File(path);
+			final fileContents = await file.readAsString();
 			final contentType = mime.lookupMimeType(path);
 			return Response.ok(fileContents)
 				..contentType = io.ContentType.parse(contentType);
